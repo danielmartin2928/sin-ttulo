c97ac2c2a905da6e83cbb2427e264ea812d28643
danielmartin2928 <122784748+danielmartin2928@users.noreply.github.com>
2023-01-16 11:24:59 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+let nave = game.createSprite(2, 4)
+let enemigo1 = game.createSprite(2, 0)
+let disparo = game.createSprite(2, 0)
+enemigo1.delete()
+disparo.delete()
+game.setLife(0)
+basic.forever(function () {
+	
+})
